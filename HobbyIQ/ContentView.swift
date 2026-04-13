2fca1fdd4868a55d69dbfe76eae0ec848c4e2db3
Drew Vabulas <dvabulas@outlook.com>
2026-04-12 20:39:37 -0400

    Initial Commit

---
+++ HobbyIQ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HobbyIQ
+//
+//  Created by Drew Vabulas on 4/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HobbyIQ/HobbyIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobbyIQApp.swift
+//  HobbyIQ
+//
+//  Created by Drew Vabulas on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct HobbyIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
